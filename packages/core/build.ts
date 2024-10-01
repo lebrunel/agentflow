8d918b8d842a94f616e77d7864f6c8213f70c889
lebrunel <124721263+lebrunel@users.noreply.github.com>
2024-10-01 18:24:36 +0100

    core build types with tsc, bundle with bun

--- packages/core/build.ts
+++ packages/core/build.ts
@@ -1,4 +1,16 @@
-await Bun.build({
+// Node bundle
+Bun.build({
+  entrypoints: ['./src/index.ts'],
+  outdir: './dist',
+  format: 'esm',
+  target: 'node',
+  packages: 'external',
+  sourcemap: 'external',
+  minify: false,
+})
+
+// Browser bundle
+Bun.build({
   entrypoints: ['./src/index.ts'],
   outdir: './dist',
   format: 'esm',

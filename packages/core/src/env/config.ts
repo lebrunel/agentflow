8b02b610d3188f25c157ab45948eb5ac0d648378
lebrunel <124721263+lebrunel@users.noreply.github.com>
2024-11-24 20:04:25 +0000

    env namespace

---
+++ packages/core/src/env/config.ts
@@ -0,0 +1,22 @@
+import type { Provider } from 'ai'
+import type { z } from 'zod'
+
+import type { Plugin } from './environment'
+import type { Action } from '../action'
+import type { Tool } from '../tool'
+
+/**
+ * TODO
+ */
+export function defineConfig(config: UserConfig | (() => UserConfig)): UserConfig {
+  return typeof config === 'function' ? config() : config
+}
+
+// Types
+
+export interface UserConfig {
+  actions?: Action[];
+  tools?: Tool<z.ZodType>[];
+  providers?: Record<string, Provider>;
+  plugins?: Plugin[];
+}
